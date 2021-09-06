af0f6848921909651856cd1a8de7b6ebed69734a
lucy0828 <0828lucy@gmail.com>
2021-09-06 17:30:15 +0900

    gitignore work

---
+++ RaspiSpeechRecognition/codegen/exe/HelperSpeechCommandRecognitionRasPi/interface/_coder_HelperSpeechCommandRecognitionRasPi_info.h
@@ -0,0 +1,25 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: _coder_HelperSpeechCommandRecognitionRasPi_info.h
+//
+// MATLAB Coder version            : 5.2
+// C/C++ source code generated on  : 07-Jul-2021 18:07:43
+//
+
+#ifndef _CODER_HELPERSPEECHCOMMANDRECOGNITIONRASPI_INFO_H
+#define _CODER_HELPERSPEECHCOMMANDRECOGNITIONRASPI_INFO_H
+
+// Include Files
+#include "mex.h"
+
+// Function Declarations
+MEXFUNCTION_LINKAGE mxArray *emlrtMexFcnProperties();
+
+#endif
+//
+// File trailer for _coder_HelperSpeechCommandRecognitionRasPi_info.h
+//
+// [EOF]
+//
